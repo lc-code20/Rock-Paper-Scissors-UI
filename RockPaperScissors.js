378d64536c35dae914a10b6d06fdb8999cc4f2b0
lc-code20 <62159627+lc-code20@users.noreply.github.com>
2020-09-11 21:07:51 +0100

    separate html and js

---
+++ RockPaperScissors.js
@@ -0,0 +1,76 @@
+console.log("Start!")
+
+    let moves = ['rock','paper','scissors']
+
+    function computerPlay(){
+      return moves[Math.floor(Math.random()*3)];
+    }
+    
+    function rulesWin(move){
+      if (move==='rock'){
+        return 'Rock beats Paper';
+      }
+      if (move==='paper'){
+        return 'Paper beats Rock';
+      }
+      else {
+        return 'Scissors beats Paper';
+      }
+    }
+
+    function playRound(playerSelection, computerSelection){
+
+      console.log('Your move is: ' + playerSelection + '. Pc is: ' + computerSelection);
+
+      if (playerSelection===computerSelection){
+        return 'It is a draw!' 
+      }
+      if (playerSelection==='rock' && computerSelection==='scissors' ||
+        playerSelection==='paper' && computerSelection==='rock'||
+        playerSelection==='scissors' && computerSelection==='paper'){
+        playerPoints++;
+        return ('You win! ' + rulesWin(playerSelection));
+      }
+      else {
+        computerPoints++;
+        return ('You lose. ' + rulesWin(computerSelection));
+      }
+
+    }
+
+    let playerPoints = 0
+    let computerPoints = 0
+
+    function game(){
+      
+
+      for (i=0;i<5;i++){
+        console.log('Round '+ (i+1))
+
+        let input = window.prompt("Your move: ").toLowerCase();
+        
+        while (input != 'rock'&&
+          input != 'paper'&&
+          input != 'scissors'){
+          input = window.prompt("Your move needs to be rock, paper or scissors: ").toLowerCase();
+        }
+
+        const computerSelection = computerPlay();
+
+        console.log(playRound(input, computerSelection))
+
+      }
+
+      console.log('Game end ');
+
+      if (playerPoints>computerPoints){
+        console.log('You win!')
+      } else if (playerPoints === computerPoints){
+        console.log('Its a draw!')
+      } else {
+        console.log("You lose")
+      }
+
+      console.log('Your points: ' + playerPoints + '. Computer points: ' + computerPoints)
+
+    }
